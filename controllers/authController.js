311d272d7709d64158b5258f2c18a11086076d1a
Caleno-R <calenorose@gmail.com>
2022-03-08 10:45:13 +0300

    added auth controller, user schema and database config

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+exports.login = async (req, res) => {
+    try {
+         
+    }catch(err) {
+        res.status(500).json(err)
+    }
+}
