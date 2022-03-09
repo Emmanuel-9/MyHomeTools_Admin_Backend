885f4b86a2754c1d9bc436c24e002416bc790b69
IshaqKassam <ishaq.kassam@gmail.com>
2022-03-09 10:05:25 +0300

    doing tests

--- controllers/authController.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const authController = require( './authController' )
-
-describe( 'POST /auth/login', () => {
-    test( 'it should respond with user object and token', () => {
-        const response = await request(authController)
-    })
-})
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 exports.login = async (req, res) => {
     try {
-         
+         res.status(200)
     }catch(err) {
         res.status(500).json(err)
     }
