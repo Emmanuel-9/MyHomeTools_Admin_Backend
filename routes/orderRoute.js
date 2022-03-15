24f86592ddcd66bee8f248fa45908e1a5d6a5764
IshaqKassam <ishaq.kassam@gmail.com>
2022-03-15 11:57:23 +0300

    /GET order by id API done - to test when data is present

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const orderController = require( '../controllers/orderController' )
 
 router.get( '/', orderController.findAll )
+router.get( '/:id', orderController.findOne )
 
 module.exports = router
